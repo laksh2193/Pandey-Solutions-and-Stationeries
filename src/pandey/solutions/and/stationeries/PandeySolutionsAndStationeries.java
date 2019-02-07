5c4c202a61851aa609e44f7af354339907c8aff6
laskh2193 <monukumar8745804276@gmail.com>
2019-02-07 18:29:54 +0530

    shop project

---
+++ src/pandey/solutions/and/stationeries/PandeySolutionsAndStationeries.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pandey.solutions.and.stationeries;
+
+/**
+ *
+ * @author satish
+ */
+public class PandeySolutionsAndStationeries {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
